ccc9869dabc5336ff7b8bb9213547afe58377aa1
Gabriel Henrique <gabhenrique0@gmail.com>
2022-03-25 09:35:59 -0300

    :sparkles: Feat: add a stepper and date picker on app

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount: Double = 8.0
+    @State private var wakeUp: Date = .now
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            
+            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                .labelsHidden()
+        }
+        .padding()
     }
 }
 
