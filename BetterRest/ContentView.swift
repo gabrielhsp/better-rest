a4dc51d959e74c0cf9558413c21afd1976449b48
Gabriel Henrique <gabhenrique0@gmail.com>
2022-03-28 08:42:42 -0300

    :lipstick: Feat: create the UI basic app structure

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,17 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount: Double = 8.0
+    // MARK: - Property Wrappers
     @State private var wakeUp: Date = .now
+    @State private var sleepAmount: Double = 8.0
+    @State private var coffeeAmount: Int = 1
     
+    // MARK: - UI Components
     var body: some View {
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            
-            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                .labelsHidden()
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount) \(coffeeAmount == 1 ? "cup" : "cups")", value: $coffeeAmount, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
         }
-        .padding()
+    }
+    
+    // MARK: - Public and Internal Methods
+    func calculateBedTime() {
+        
     }
 }
 
