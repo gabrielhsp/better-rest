e5a72ff6887b287ea266ab04be0d35204e0b0f94
Gabriel Henrique <gabhenrique0@gmail.com>
2022-03-28 09:00:46 -0300

    :sparkles: Feat: integrate core ml prediction with user input data

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Gabriel Pereira on 25/03/22.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -12,6 +13,9 @@ struct ContentView: View {
     @State private var wakeUp: Date = .now
     @State private var sleepAmount: Double = 8.0
     @State private var coffeeAmount: Int = 1
+    @State private var alertTitle: String = ""
+    @State private var alertMessage: String = ""
+    @State private var showingAlert: Bool = false
     
     // MARK: - UI Components
     var body: some View {
@@ -37,12 +41,40 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedTime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     
     // MARK: - Public and Internal Methods
     func calculateBedTime() {
+        do {
+            let config: MLModelConfiguration = .init()
+            let model: SleepCalculator = try .init(configuration: config)
+            
+            let components: DateComponents = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour: Int = (components.hour ?? 0) * 60 * 60
+            let minute: Int = (components.minute ?? 0) * 60
+            
+            let prediction: SleepCalculatorOutput = try model.prediction(
+                wake: Double(hour + minute),
+                estimatedSleep: sleepAmount,
+                coffee: Double(coffeeAmount)
+            )
+            
+            let sleepTime: Date = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime"
+        }
         
+        showingAlert = true
     }
 }
 
